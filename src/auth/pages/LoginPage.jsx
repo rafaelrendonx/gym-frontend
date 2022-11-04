5a4bead09605869ad91ec0844dd5c610a901c283
rafaelrendox <rafael_x@hotmail.com>
2022-11-03 20:19:33 -0700

    Paginas de registro y login agregadas

--- src/auth/pages/LoginPage.jsx
+++ src/auth/pages/LoginPage.jsx
@@ -1,6 +1,49 @@
+import { useState } from 'react';
+import { useForm } from '../../hooks/useForm';
 
 export const LoginPage = () => {
+  const formValues = {
+    email: '',
+    password: '',
+  };
+  const { formState, onInputChange, email, password } = useForm(formValues);
+  const [loggedUser, setLoggedUser] = useState({})
+
+  const onSubmit = (event) => {
+    event.preventDefault()
+    setLoggedUser([
+      formState
+    ])
+    console.log(loggedUser)
+  }
+
   return (
-    <div>LoginPage</div>
-  )
-}
+    <>
+      <h1>LoginPage</h1>
+      <hr />
+      <form onSubmit={onSubmit}>
+        <p>Email</p>
+        <input
+          placeholder="Correo@hotmail.com"
+          name="email"
+          className="form-control mb-5 w-25 "
+          type="text"
+          onChange={onInputChange}
+          value={email}
+        />
+        <p>Contraseña</p>
+        <input
+          placeholder="Contraseña"
+          name="password"
+          className="form-control mb-5 w-25 "
+          type="password"
+          onChange={onInputChange}
+          value={password}
+        />
+        <button type="submit" className="btn btn-primary">
+          Login
+        </button>
+      </form>
+    </>
+  );
+};
--- src/auth/pages/RegisterPage.jsx
+++ src/auth/pages/RegisterPage.jsx
@@ -1,6 +1,60 @@
+import { useState } from "react";
+import { useForm } from "../../hooks/useForm";
 
 export const RegisterPage = () => {
+  const formValues = {
+    nombre: '',
+    email: '',
+    password: '',
+  };
+  const { formState, onInputChange, email, password , nombre} = useForm(formValues);
+  const [registeredUser, setRegisteredUser] = useState({})
+
+  const onSubmit = (event) => {
+    event.preventDefault()
+    setRegisteredUser([
+      formState
+    ])
+    console.log(registeredUser)
+  }
   return (
-    <div>RegisterPage</div>
-  )
-}
+    <>
+      <h1>Register Page</h1>
+      <hr />
+      <form onSubmit={onSubmit}>
+        <p>Nombre</p>
+        <input
+          placeholder="Nombre completo"
+          name="nombre"
+          className="form-control mb-5 w-25 "
+          type="text"
+          onChange={onInputChange}
+          value={nombre}
+        />
+        <p>Email</p>
+        <input
+          placeholder="Correo@hotmail.com"
+          name="email"
+          className="form-control mb-5 w-25 "
+          type="text"
+          onChange={onInputChange}
+          value={email}
+
+        />
+        <p>Contraseña</p>
+        <input
+          placeholder="Contraseña"
+          name="password"
+          className="form-control mb-5 w-25 "
+          type="password"
+          onChange={onInputChange}
+          value={password}
+
+        />
+        <button type="submit" className="btn btn-primary">
+          Registrarme
+        </button>
+      </form>
+    </>
+  );
+};
